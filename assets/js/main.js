7d11343e976bacd5c30dd5df886d4c41bbb3f842
Wasley Araújo <waslleymax@gmail.com>
2022-08-16 20:11:16 -0300

    :recycle: applying the SRP

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,34 +5,43 @@ const elementBtn = document.querySelector('#btn');
 elementBtn.addEventListener('click', (event) => {
   event.preventDefault();
   const nameAuthors = document.querySelector('#name').value;
+  const result = document.querySelector('.list');
+
   if (nameAuthors === '') {
     alert('Digite o nome do autor');
     return;
   }
-  const result = document.querySelector('.list');
-  const booksAuthors = booksByCategory
-    .map(category => category.books)
-    .map(books => books
-      .filter(book => book.author
-        .toLowerCase()
-        .startsWith(nameAuthors.toLowerCase())))
-      
-  const consultBook = booksAuthors.filter(books => books.length > 0);
+  
+  function getBooksByAuthor() {
+    const books = booksByCategory.reduce((acc, category) => {
+      return [...acc, ...category.books];
+    }, []);
 
-  if (consultBook.length > 0) {
-    result.innerHTML = '';
-    consultBook.forEach(books => {
-      books.forEach(book => {
-        const li = document.createElement('li');
-        li.innerHTML = `
-        <img src="assets/img/book_black_24dp.svg" alt="">
-        ${book.title} - ${book.author}
-        `;
-        result.appendChild(li);
-      })
-    })
-  } else {
-    result.innerHTML = 'Não há livros deste autor';
+    return books
   }
 
-})
+  function filterBooksByAuthor(getBooksByAuthor) {
+    return getBooksByAuthor().filter((book) => book.author
+    .toLowerCase().startsWith(nameAuthors.toLowerCase()));
+  }
+  
+  const books = filterBooksByAuthor(getBooksByAuthor);
+
+  if (books.length === 0) {
+    result.innerHTML = 'Nenhum livro encontrado';
+    return;
+  }
+
+  result.innerHTML = '';
+  const ul = document.createElement('ul');
+  result.appendChild(ul);
+
+  books.forEach((book) => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+    <img src="assets/img/book_black_24dp.svg" alt="">
+    <p>${book.title}</p>
+    `
+    ul.appendChild(li);
+  });
+});
